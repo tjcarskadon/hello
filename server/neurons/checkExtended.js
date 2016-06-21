f6162017fe2be12bc80a02cef98fe992e7fba608
Prateek Madapurmath <prateekm33@gmail.com>
2016-06-20 18:18:11 -0700

    (feat)-continue modularization

--- server/neurons/checkExtended.js
+++ server/neurons/checkExtended.js
@@ -4,13 +4,15 @@ var net = new brain.NeuralNetwork();
 
 //1 is true; 0 is false
 net.train([
-  {input: [0, 0, 0, 0, 0] , output: {extended: 0}},
-  {input: [1, 0, 0, 0, 0], output: {extended: 1}},
-  {input: [0, 1, 0, 0, 0], output: {extended: 1}},
-  {input: [0, 0, 1, 0, 0], output: {extended: 1}},
-  {input: [0, 0, 0, 1, 0], output: {extended: 1}},
-  {input: [0, 0, 0, 0, 1], output: {extended: 1}},
+  {input: [0, 0, 0, 0, 0] , output: {false: 0}},
+  {input: [1, 0, 0, 0, 0], output: {true: 1}},
+  {input: [0, 1, 0, 0, 0], output: {true: 1}},
+  {input: [0, 0, 1, 0, 0], output: {true: 1}},
+  {input: [0, 0, 0, 1, 0], output: {true: 1}},
+  {input: [0, 0, 0, 0, 1], output: {true: 1}},
 ]);
+
+module.exports = net;
 
 //if net.run(DATA) returns a larger % for extended, then run a function to check the extended branch
   //else run a function to check the non-extended branch
