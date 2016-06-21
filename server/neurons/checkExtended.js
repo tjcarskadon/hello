6f597c0a90663bb4f5bd134036a49a54a28cfa42
Prateek Madapurmath <prateekm33@gmail.com>
2016-06-20 17:06:16 -0700

    (feat) - begin modularization of neuron code

---
+++ server/neurons/checkExtended.js
@@ -0,0 +1,16 @@
+var brain = require('brain');
+
+var net = new brain.NeuralNetwork();
+
+//1 is true; 0 is false
+net.train([
+  {input: [0, 0, 0, 0, 0] , output: {extended: 0}},
+  {input: [1, 0, 0, 0, 0], output: {extended: 1}},
+  {input: [0, 1, 0, 0, 0], output: {extended: 1}},
+  {input: [0, 0, 1, 0, 0], output: {extended: 1}},
+  {input: [0, 0, 0, 1, 0], output: {extended: 1}},
+  {input: [0, 0, 0, 0, 1], output: {extended: 1}},
+]);
+
+//if net.run(DATA) returns a larger % for extended, then run a function to check the extended branch
+  //else run a function to check the non-extended branch
