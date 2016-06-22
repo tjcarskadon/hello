ad4ff297b20c03fa96848b0847a0c0fa207615d3
Tj Carskadon <tjcarskadon@gmail.com>
2016-06-22 12:15:11 -0700

    (feat) -Build rest of neuron/DTree schema
    Testing in progress

--- server/neurons/isMiddleDown.js
+++ server/neurons/isMiddleDown.js
@@ -5,7 +5,7 @@ var net = new brain.NeuralNetwork();
 //Check difference between middle tip Y and index knuckle Y -  large means middle facing down
 net.train([
   {input:[74.268], output:{true: 1}}, 
-  {input:[8.151], output:{false: 0}}
+  {input:[8.151], output:{false: 1}}
 ]);
 
 module.exports = net;
--- server/neurons/isRotated.js
+++ server/neurons/isRotated.js
@@ -4,8 +4,8 @@ var net = new brain.NeuralNetwork();
 
 //CHeck roatation of hand 
 net.train([
-  {input:[36.32599999999999], output:{true: 1}}, 
-  {input:[-7.2379999999999995], output:{false: 0}}
+  {input:[-47.666], output:{true: 1}}, 
+  {input:[12.115], output:{false: 1}}
 ]);
 
 module.exports = net;
