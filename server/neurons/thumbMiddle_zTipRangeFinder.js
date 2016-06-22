49b695dad214a252da5b2ceeb6e9962ccaf86a44
Tj Carskadon <tjcarskadon@gmail.com>
2016-06-22 15:27:12 -0700

    (feat) - Build rest of neuron/DTree schema
    Improve D vs. X recognition

--- server/neurons/thumbMiddle_zTipRangeFinder.js
+++ server/neurons/thumbMiddle_zTipRangeFinder.js
@@ -4,8 +4,8 @@ var net = new brain.NeuralNetwork();
 
 //Check difference between thumb tip Z and middle tip Z.  If small then D
 net.train([
-  {input:[20.91126], output:{true: 1}}, 
-  {input:[32.8292], output:{false: 1}}
+  {input:[-23.054], output:{true: 1}}, 
+  {input:[46.468], output:{false: 1}}
 ]);
 
 module.exports = net;
