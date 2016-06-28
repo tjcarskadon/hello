3f9d7f50894fa9de961c017431c77cdb12fe2ff4
Tj Carskadon <tjcarskadon@gmail.com>
2016-06-28 14:04:14 -0700

    (feat) add welcome page basic styling

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -2,9 +2,14 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'welcome',
-  template: require('./welcome.component.html')
+  template: require('./welcome.component.html'),
+  styles: [
+    require('./welcome.component.css')
+  ]
 })
 export class Welcome implements OnInit {
+
+  hands: string = 'assets/img/hands.png'
 
   constructor() {}
 
