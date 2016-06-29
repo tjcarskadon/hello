e1209e2f7d7afce77ad655dc2ced63d937de486f
Tj Carskadon <tjcarskadon@gmail.com>
2016-06-28 19:48:28 -0700

    (feat) add login page and set up basic routin
    Bug - cancel on the login page rerenders the welcom page compeletly low
    priority but would be nice to fix

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { WelcomeContent } from '../welcomeContent/welcomeContent.ts';
 import { Signup } from '../signup/signup.ts'
+import { Login } from '../login/login.ts'
 import { AppState } from '../app.service';
 @Component({
   selector: 'welcome',
-  directives: [ WelcomeContent, Signup ],
+  directives: [ WelcomeContent, Signup, Login ],
   template: require('./welcome.component.html'),
   styles: [
     require('./welcome.component.css')
@@ -13,7 +14,7 @@ import { AppState } from '../app.service';
 export class Welcome implements OnInit {
 
   hands: string = 'assets/img/hands.png'
-
+  bg: string = 'assets/img/bg.png'
   constructor(public appState: AppState) {}
 
   ngOnInit() {}
