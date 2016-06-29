b1823778590680a7c6f21b89a931e358e798178f
Ashwini <ashjd29@gmail.com>
2016-06-28 20:34:49 -0700

    (feat) Alphabets and images working

--- src/app/learn/learn.component.ts
+++ src/app/learn/learn.component.ts
@@ -2,13 +2,22 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'learn',
-  template: require('./learn.component.html')
+  template: require('./learn.component.html'),
+  styles: [require('./learn.component.css')],
 })
 export class Learn implements OnInit {
+
+  public imageUrl:string = '';
+  public clickedLtr:string;
 
   constructor() {}
 
   ngOnInit() {
+  }
+
+  clicked(ltr) {
+    this.imageUrl = `assets/img/${ltr}.png`;
+    this.clickedLtr = ltr;
   }
 
 }
