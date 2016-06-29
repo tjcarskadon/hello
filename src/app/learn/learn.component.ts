74cca308c1a21318c2cce91b83ff01260520b1db
Ashwini <ashjd29@gmail.com>
2016-06-29 14:10:43 -0700

    divs show/hide working

--- src/app/learn/learn.component.ts
+++ src/app/learn/learn.component.ts
@@ -9,17 +9,34 @@ export class Learn implements OnInit {
 
   public imageUrl:string = '';
   public clickedLtr:string;
-  public timedOut = true;
+  public showImgDiv:boolean = false;
+  public showCaptureDiv:boolean = false;
+  public color:string = 'blue';
+  public score:number = 0;
 
   constructor() {}
 
   ngOnInit() {
   }
 
+  changeLetterColor() {
+
+  }
+
   clicked(ltr) {
     this.imageUrl = `assets/img/${ltr}.png`;
     this.clickedLtr = ltr;
-    this.timedOut = false;
+    this.showImgDiv = true;
+  }
+
+  hideImgDiv() {
+    this.showImgDiv = false;
+    this.showCaptureDiv = true;
+  }
+
+  hideCaptureDiv() {
+    this.showCaptureDiv = false;
+    this.changeLetterColor();
   }
 
 }
