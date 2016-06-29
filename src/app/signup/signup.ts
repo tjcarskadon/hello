ab46fe3cf0fde930ed22c12e9d5d4137a7b8c039
Tj Carskadon <tjcarskadon@gmail.com>
2016-06-29 16:20:57 -0700

    (feat) - Local Authentication In progress - basic (local only)  user sign up form submission done

--- src/app/signup/signup.ts
+++ src/app/signup/signup.ts
@@ -1,16 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { AppState } from '../app.service';
+import { FormBuilder, Validators, FORM_DIRECTIVES, ControlGroup } from '@angular/common';
 
 @Component({
   selector: 'signup',
+  directives: [ FORM_DIRECTIVES ],
   template: require('./signup.html'),
   styles: [require('./signup.css')]
 })
 
 export class Signup implements OnInit {
-  constructor(public appState: AppState) {}
+  signupForm: ControlGroup;
+
+  constructor(public appState: AppState, fb: FormBuilder) {
+    this.signupForm = fb.group({
+      'email': [],
+      'password': [],
+      'confirm': []
+    })
+  }
   ngOnInit() {}  
 
+  onSubmit(form: any) {
+    console.log('success', form);
+  }
   loginRoute () {
     this.appState.login = true;
     this.appState.signup = false;
