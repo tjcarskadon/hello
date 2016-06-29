d5c54bdbc85107918be3dc6c9d8d4cf00e42e574
Tj Carskadon <tjcarskadon@gmail.com>
2016-06-28 17:15:17 -0700

    (feat) links welcome to signup and learn to the authenticated learn page

--- src/app/welcomeContent/welcomeContent.ts
+++ src/app/welcomeContent/welcomeContent.ts
@@ -1,13 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { AppState } from '../app.service';
+import { RouterActive } from '../router-active';
+
 @Component({
   selector: 'welcomeContent',
+  directives: [ RouterActive ],
   template: require('./welcomeContent.html'),
   styles: [require('./welcomeContent.css')]
 })
 
 export class WelcomeContent implements OnInit {
-  constructor() {}
+  constructor(public appState: AppState) {}
 
   ngOnInit() {}
 
