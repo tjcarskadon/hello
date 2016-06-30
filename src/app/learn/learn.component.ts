991499a0d0dfe53f31c2f927f9431a2d52e2cc9b
Ashwini <ashjd29@gmail.com>
2016-06-29 18:40:41 -0700

    color change not working

--- src/app/learn/learn.component.ts
+++ src/app/learn/learn.component.ts
@@ -11,8 +11,36 @@ export class Learn implements OnInit {
   public clickedLtr:string;
   public showImgDiv:boolean = false;
   public showCaptureDiv:boolean = false;
-  public color:string = 'blue';
+  public color:string = 'warn';
   public score:number = 0;
+  public letters = [
+    {val: 'A', color:'primary'}, 
+    {val: 'B', color:'primary'}, 
+    {val: 'C', color:'primary'}, 
+    {val: 'D', color:'primary'}, 
+    {val: 'E', color:'primary'}, 
+    {val: 'F', color:'primary'}, 
+    {val: 'G', color:'primary'}, 
+    {val: 'H', color:'primary'}, 
+    {val: 'I', color:'primary'}, 
+    {val: 'J', color:'primary'}, 
+    {val: 'K', color:'primary'}, 
+    {val: 'L', color:'primary'}, 
+    {val: 'M', color:'primary'}, 
+    {val: 'N', color:'primary'}, 
+    {val: 'O', color:'primary'}, 
+    {val: 'P', color:'primary'}, 
+    {val: 'Q', color:'primary'}, 
+    {val: 'R', color:'primary'}, 
+    {val: 'S', color:'primary'}, 
+    {val: 'T', color:'primary'}, 
+    {val: 'U', color:'primary'}, 
+    {val: 'V', color:'primary'}, 
+    {val: 'W', color:'primary'}, 
+    {val: 'X', color:'primary'}, 
+    {val: 'Y', color:'primary'}, 
+    {val: 'Z', color:'primary'}
+  ];
 
   constructor() {}
 
@@ -20,10 +48,12 @@ export class Learn implements OnInit {
   }
 
   changeLetterColor() {
-
+    var idx = (this.clickedLtr.charCodeAt()) - 65;
+    this.letters[idx].color = 'warn';
   }
 
   clicked(ltr) {
+    ltr = ltr.toLowerCase();
     this.imageUrl = `assets/img/${ltr}.png`;
     this.clickedLtr = ltr;
     this.showImgDiv = true;
@@ -37,6 +67,7 @@ export class Learn implements OnInit {
   hideCaptureDiv() {
     this.showCaptureDiv = false;
     this.changeLetterColor();
+
   }
 
 }
