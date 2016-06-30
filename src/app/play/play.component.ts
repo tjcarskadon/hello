5353ecc3b72325df88086434992f83348788d26d
Richard May <mybrainishuge@users.noreply.github.com>
2016-06-30 16:47:58 -0700

    (feat) Add profile, styling

--- src/app/play/play.component.ts
+++ src/app/play/play.component.ts
@@ -2,8 +2,10 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'play',
-  template: require('./play.component.html')
+  template: require('./play.component.html'),
+  styles: [require('./play.component.css')]
 })
+
 export class Play implements OnInit {
 
   constructor() {}
